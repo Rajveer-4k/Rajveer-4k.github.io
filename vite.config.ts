b4e5a5eb94d8453330e2ac0777f101d44ecafc25
Professional <kapoorrajveer@icloud.com>
2026-02-26 10:24:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  // This MUST be your exact repo name with slashes on both sides
-  base: '/Rajveer-4k.github.io/', 
+  plugins: [react(), tailwindcss()],
+  // Change this from a specific name to just './' 
+  // This tells Vite: "Look for assets relative to wherever index.html is"
+  base: './', 
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
   }
 })
